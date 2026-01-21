2622cc822ee7dfb2afb22c81ac29410db77d63ca
Gabriel Souza <gs8526368@gmail.com>
2026-01-21 20:14:53 -0300

    base

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-       int N= int.Parse(Console.ReadLine());
-    if (N < 0){System.Console.WriteLine("NEGATIVO");
-       }
-        else{System.Console.WriteLine("NAO NEGATIVO");}
+
     }
 }
