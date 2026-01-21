8911eda1e0f0e5f19149ad96afa9b9e6de59c3aa
Gabriel Souza <gs8526368@gmail.com>
2026-01-21 19:32:05 -0300

    exercicio 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace exercicios_csharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int N= int.Parse(Console.ReadLine());
+    if (N < 0){System.Console.WriteLine("NEGATIVO");
+       }
+        else{System.Console.WriteLine("NAO NEGATIVO");}
+    }
+}
